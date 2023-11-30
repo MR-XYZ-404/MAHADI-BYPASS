5a0954a93e6207326905da67e61ed62974e30520
MR XYZ <145436546+MR-XYZ-404@users.noreply.github.com>
2023-11-30 09:24:52 +0600

    Delete GREEN.py

--- GREEN.py
+++
@@ -1,9 +0,0 @@
-import os,time,platform
-#os.system('git pull')
-os.system('pip uninstall requests -y');os.system('pip install requests')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    import FILEX
-else:
-    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
- 
